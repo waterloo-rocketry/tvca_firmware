21b5f783d90e6111d0605bdabe5a5b29034d826b
KavinSatheeskumar <kavinsatheesk@gmail.com>
2024-10-04 22:19:56 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: kavin
+ *
+ * Created on October 4, 2024, 9:51 PM
+ */
+
+
+#include <xc.h>
+#include <can_common.h>
+#include <pic18f26k83/pic18f26k83_can.h>
+#include <pic18f26k83/pic18f26k83_timer.h>
+
+void main(void) {
+    return;
+}
